25e69845b575b896931547a0f1782014fe91a8f2
54v054r3n <17554782+haschi@users.noreply.github.com>
2023-03-13 15:25:57 +0100

    refactoring http handler

--- recipe/src/server.rs
+++ recipe/src/server.rs
@@ -1,28 +1,16 @@
-use std::{
-    ops::Bound,
-    sync::{Arc, RwLock},
-};
+use std::sync::{Arc, RwLock};
 
-use axum::{
-    extract::{Json, Path, Query, State, TypedHeader},
-    headers::Range,
-    http::{header, StatusCode},
-    response::IntoResponse,
-    routing, Router,
-};
-use recipers::{
-    repository::{Repository, UpdateResult},
-    Recipe,
-};
-
-use serde::{Deserialize, Serialize};
-use uuid::Uuid;
+use axum::{routing, Router};
+use recipers::repository::Repository;
 
 use tower_http::trace::TraceLayer;
 use tracing_subscriber::{layer::SubscriberExt, util::SubscriberInitExt};
+
+use crate::handler::{
+    recipe_delete, recipe_get, recipe_put, recipe_share, recipes_get, recipes_post,
+};
 // use cookbook::recipe_service_server::{RecipeService, RecipeServiceServer};
 // use cookbook::{ListTableOfContentsRequest, TableOfContentsResponse};
-
 // use tonic::{transport::Server, Request, Response, Status};
 
 // pub mod cookbook {
@@ -105,151 +93,168 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
 
 type AppState = Arc<RwLock<Repository>>;
 
-#[derive(Debug, Deserialize)]
-struct Search {
-    q: Option<String>,
-}
+mod handler {
+    use std::ops::Bound;
 
-#[derive(Serialize)]
-struct TableOfContents {
-    total: u64,
-    content: Vec<Content>,
-}
+    use axum::{
+        extract::{Path, Query, State},
+        headers::Range,
+        http::{header, StatusCode},
+        response::IntoResponse,
+        Json, TypedHeader,
+    };
+    use recipers::{repository::UpdateResult, Recipe};
+    use serde::{Deserialize, Serialize};
+    use uuid::Uuid;
 
-impl From<&(&recipers::TableOfContents, &Vec<&str>)> for TableOfContents {
-    fn from(value: &(&recipers::TableOfContents, &Vec<&str>)) -> Self {
-        let toc = value.0;
-        let path = value.1;
+    use crate::AppState;
 
-        let x = toc
-            .content
+    #[derive(Debug, Deserialize)]
+    pub struct Search {
+        q: Option<String>,
+    }
+
+    #[derive(Serialize)]
+    struct TableOfContents {
+        total: u64,
+        content: Vec<Content>,
+    }
+
+    impl From<&(&recipers::TableOfContents, &Vec<&str>)> for TableOfContents {
+        fn from(value: &(&recipers::TableOfContents, &Vec<&str>)) -> Self {
+            let toc = value.0;
+            let path = value.1;
+
+            let x = toc
+                .content
+                .iter()
+                .map(|item| Content {
+                    title: item.title.clone(),
+                    recipe_id: item.id,
+                    links: Links::new(item.id, path),
+                })
+                .collect();
+
+            TableOfContents {
+                total: toc.total,
+                content: x,
+            }
+        }
+    }
+
+    #[derive(Serialize)]
+    struct Content {
+        title: String,
+        #[serde(rename = "recipeId")]
+        recipe_id: Uuid,
+        #[serde(rename = "_links")]
+        links: Links,
+    }
+
+    #[derive(Serialize)]
+    struct Links {
+        #[serde(rename = "self")]
+        myself: Link,
+    }
+
+    impl Links {
+        fn new(id: Uuid, path: &[&str]) -> Links {
+            let href = format!(
+                "http://localhost:8181/{}",
+                [path.join("/"), id.to_string()].join("/")
+            );
+
+            Links {
+                myself: Link { href },
+            }
+        }
+    }
+
+    #[derive(Serialize)]
+    struct Link {
+        href: String,
+    }
+
+    pub async fn recipes_get(
+        State(state): State<AppState>,
+        Query(parameter): Query<Search>,
+        TypedHeader(range): TypedHeader<Range>,
+    ) -> Result<impl IntoResponse, (StatusCode, String)> {
+        let search = parameter.q.unwrap_or("".into());
+
+        let it: (Bound<u64>, Bound<u64>) = range
             .iter()
-            .map(|item| Content {
-                title: item.title.clone(),
-                recipe_id: item.id,
-                links: Links::new(item.id, path),
-            })
-            .collect();
+            .nth(0)
+            .unwrap_or((Bound::Unbounded, Bound::Unbounded));
 
-        TableOfContents {
-            total: toc.total,
-            content: x,
+        for r in range.iter() {
+            tracing::debug!("found range {:?}", r)
         }
-    }
-}
 
-#[derive(Serialize)]
-struct Content {
-    title: String,
-    #[serde(rename = "recipeId")]
-    recipe_id: Uuid,
-    #[serde(rename = "_links")]
-    links: Links,
-}
-
-#[derive(Serialize)]
-struct Links {
-    #[serde(rename = "self")]
-    myself: Link,
-}
-
-impl Links {
-    fn new(id: Uuid, path: &[&str]) -> Links {
-        let href = format!(
-            "http://localhost:8181/{}",
-            [path.join("/"), id.to_string()].join("/")
-        );
-
-        Links {
-            myself: Link { href },
-        }
-    }
-}
-
-#[derive(Serialize)]
-struct Link {
-    href: String,
-}
-
-async fn recipes_get(
-    State(state): State<AppState>,
-    Query(parameter): Query<Search>,
-    TypedHeader(range): TypedHeader<Range>,
-) -> Result<impl IntoResponse, (StatusCode, String)> {
-    let search = parameter.q.unwrap_or("".into());
-
-    let it: (Bound<u64>, Bound<u64>) = range
-        .iter()
-        .nth(0)
-        .unwrap_or((Bound::Unbounded, Bound::Unbounded));
-
-    for r in range.iter() {
-        tracing::debug!("found range {:?}", r)
+        let repository = state.read().unwrap();
+        let toc = repository.list2(&it, &search).map_err(internal_error)?;
+        let path = &vec!["cookbook", "recipe"];
+        let pair = (&toc, path);
+        Ok(Json(TableOfContents::from(&pair)))
     }
 
-    let repository = state.read().unwrap();
-    let toc = repository.list2(&it, &search).map_err(internal_error)?;
-    let path = &vec!["cookbook", "recipe"];
-    let pair = (&toc, path);
-    Ok(Json(TableOfContents::from(&pair)))
-}
-
-/// Utility function for mapping any error into a `500 Internal Server Error`
-/// response.
-fn internal_error<E>(err: E) -> (StatusCode, String)
-where
-    E: std::error::Error,
-{
-    (StatusCode::INTERNAL_SERVER_ERROR, err.to_string())
-}
-
-async fn recipes_post(
-    State(state): State<AppState>,
-    Json(payload): Json<Recipe>,
-) -> Result<impl IntoResponse, (StatusCode, String)> {
-    println!("recipes post called");
-    println!("got recipe {:?}", payload);
-
-    let mut repository = state.write().unwrap();
-    let id = repository.insert(&payload).map_err(internal_error)?;
-
-    Ok((
-        StatusCode::CREATED,
-        [(header::LOCATION, format!("/cookbook/recipe/{}", id))],
-        Json(id),
-    ))
-}
-
-async fn recipe_get(
-    State(state): State<AppState>,
-    Path(id): Path<Uuid>,
-) -> Result<impl IntoResponse, (StatusCode, String)> {
-    let repository = state.read().map_err(internal_error)?;
-    let recipe = repository.get(&id).map_err(internal_error)?;
-    match recipe {
-        Some(result) => Ok(Json(result.clone())),
-        None => Err((StatusCode::NOT_FOUND, "recipe not found".to_owned())),
+    /// Utility function for mapping any error into a `500 Internal Server Error`
+    /// response.
+    fn internal_error<E>(err: E) -> (StatusCode, String)
+    where
+        E: std::error::Error,
+    {
+        (StatusCode::INTERNAL_SERVER_ERROR, err.to_string())
     }
-}
 
-async fn recipe_put(
-    State(state): State<AppState>,
-    Path(id): Path<Uuid>,
-    Json(payload): Json<Recipe>,
-) -> Result<impl IntoResponse, (StatusCode, String)> {
-    let mut repository = state.write().unwrap();
-    let result = repository.update(&id, payload).map_err(internal_error)?;
+    pub async fn recipes_post(
+        State(state): State<AppState>,
+        Json(payload): Json<Recipe>,
+    ) -> Result<impl IntoResponse, (StatusCode, String)> {
+        println!("recipes post called");
+        println!("got recipe {:?}", payload);
 
-    match result {
-        UpdateResult::Created => Ok(StatusCode::OK.into_response()),
-        UpdateResult::Changed => Ok((
+        let mut repository = state.write().unwrap();
+        let id = repository.insert(&payload).map_err(internal_error)?;
+
+        Ok((
             StatusCode::CREATED,
             [(header::LOCATION, format!("/cookbook/recipe/{}", id))],
             Json(id),
-        )
-            .into_response()),
+        ))
     }
-}
 
-async fn recipe_delete(State(_state): State<AppState>, Path(_id): Path<Uuid>) {}
-async fn recipe_share(State(_state): State<AppState>) {}
+    pub async fn recipe_get(
+        State(state): State<AppState>,
+        Path(id): Path<Uuid>,
+    ) -> Result<impl IntoResponse, (StatusCode, String)> {
+        let repository = state.read().map_err(internal_error)?;
+        let recipe = repository.get(&id).map_err(internal_error)?;
+        match recipe {
+            Some(result) => Ok(Json(result.clone())),
+            None => Err((StatusCode::NOT_FOUND, "recipe not found".to_owned())),
+        }
+    }
+
+    pub async fn recipe_put(
+        State(state): State<AppState>,
+        Path(id): Path<Uuid>,
+        Json(payload): Json<Recipe>,
+    ) -> Result<impl IntoResponse, (StatusCode, String)> {
+        let mut repository = state.write().unwrap();
+        let result = repository.update(&id, payload).map_err(internal_error)?;
+
+        match result {
+            UpdateResult::Created => Ok(StatusCode::OK.into_response()),
+            UpdateResult::Changed => Ok((
+                StatusCode::CREATED,
+                [(header::LOCATION, format!("/cookbook/recipe/{}", id))],
+                Json(id),
+            )
+                .into_response()),
+        }
+    }
+
+    pub async fn recipe_delete(State(_state): State<AppState>, Path(_id): Path<Uuid>) {}
+    pub async fn recipe_share(State(_state): State<AppState>) {}
+}
