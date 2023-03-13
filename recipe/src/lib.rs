e8de4d42971d00540a653f3fdf8f10887968be21
54v054r3n <17554782+haschi@users.noreply.github.com>
2023-03-13 17:55:40 +0100

    unit tests added for app

--- recipe/src/lib.rs
+++ recipe/src/lib.rs
@@ -15,7 +15,7 @@ struct Ingredient {
     unit: String,
 }
 
-#[derive(PartialEq, Eq, PartialOrd, Ord, Clone, Debug, Serialize)]
+#[derive(PartialEq, Eq, PartialOrd, Ord, Clone, Debug, Serialize, Deserialize)]
 pub struct Summary {
     pub title: String,
     pub id: Uuid,
@@ -30,10 +30,19 @@ impl Into<Summary> for (&Uuid, &Recipe) {
     }
 }
 
-#[derive(Debug, Serialize)]
+#[derive(Debug, Serialize, Deserialize, PartialEq)]
 pub struct TableOfContents {
     pub total: u64,
     pub content: Vec<Summary>,
+}
+
+impl TableOfContents {
+    pub fn empty() -> TableOfContents {
+        TableOfContents {
+            total: 0,
+            content: vec![],
+        }
+    }
 }
 
 #[derive(Debug, PartialEq, Clone, Deserialize, Serialize)]
--- recipe/src/server.rs
+++ recipe/src/server.rs
@@ -59,9 +59,19 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
         .with(tracing_subscriber::fmt::layer())
         .init();
 
+    let app = router();
+    tracing::debug!("listening to 0.0.0.0:8080");
+    axum::Server::bind(&"0.0.0.0:8080".parse().unwrap())
+        .serve(app.into_make_service())
+        .await?;
+
+    Ok(())
+}
+
+fn router() -> Router {
     let repository = Arc::new(RwLock::new(Repository::new()));
 
-    let app = Router::new()
+    Router::new()
         .route("/", routing::get(|| async { "Hello World!" }))
         .route(
             "/cookbook/recipe",
@@ -81,16 +91,50 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
         .route(
             "/cookbook/recipe/share",
             routing::get(recipe_share).with_state(repository.clone()),
-        );
-
-    tracing::debug!("listening to 0.0.0.0:8080");
-    axum::Server::bind(&"0.0.0.0:8080".parse().unwrap())
-        .serve(app.into_make_service())
-        .await?;
-
-    Ok(())
+        )
 }
 
 type AppState = Arc<RwLock<Repository>>;
 
 mod handler;
+
+#[cfg(test)]
+mod test {
+
+    use hyper::StatusCode;
+    use hyper::{body::to_bytes, Body, Request};
+    use recipers::TableOfContents;
+
+    use crate::router;
+
+    use tower::Service;
+    use tower::ServiceExt;
+
+    #[tokio::test]
+    async fn get_toc() {
+        let request = Request::builder().uri("/").body(Body::empty()).unwrap();
+
+        let response = router().ready().await.unwrap().call(request).await.unwrap();
+
+        assert_eq!(response.status(), StatusCode::OK)
+    }
+
+    #[tokio::test]
+    async fn get_toc2() {
+        let request = Request::builder()
+            .uri("/cookbook/recipe")
+            .header("Range", "bytes=0-9")
+            .body(Body::empty())
+            .unwrap();
+
+        let response = router().ready().await.unwrap().call(request).await.unwrap();
+
+        assert_eq!(response.status(), StatusCode::OK);
+
+        let raw_body = to_bytes(response.into_body()).await.unwrap();
+        let got: TableOfContents = serde_json::from_slice(&raw_body).unwrap();
+        println!("{:?}", got);
+
+        assert_eq!(got, TableOfContents::empty())
+    }
+}
